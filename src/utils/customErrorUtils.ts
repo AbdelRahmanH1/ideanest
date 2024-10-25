4b62d71a1a515d160cc9155d0e2419f7822fcc47
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-10-25 23:13:18 +0300

    Optimize class CustomError to take array of string

--- src/utils/customErrorUtils.ts
+++ src/utils/customErrorUtils.ts
@@ -1,7 +1,8 @@
 export class CustomError extends Error {
   public statusCode: number;
-  constructor(message: string, statusCode: number) {
-    super(message);
+  constructor(message: string | string[], statusCode: number) {
+    const errorMessage = Array.isArray(message) ? message.join(', ') : message;
+    super(errorMessage);
     this.statusCode = statusCode;
   }
 }
