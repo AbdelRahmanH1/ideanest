8355bbcefa5e8e950fba2ba7a59512246e8f531b
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-10-25 17:55:49 +0300

    Add Custom Error Class and ErrorHandle Middleware

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,14 @@
+import type { CustomError } from '@/utils/customErrorUtils.js';
+import { errorResponse } from '@/utils/reponseUtils.js';
+import type { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (
+  err: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const message = err.message || 'Something went wrong';
+  const statusCode = err.statusCode || 500;
+  errorResponse(res, statusCode, message);
+};
