bb73e885437fc9d0367d3fdc7f9a6f8af60feae0
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-10-26 15:55:34 +0300

    Update Organization model to be  the name is unique

--- src/DB/models/organizationModel.ts
+++ src/DB/models/organizationModel.ts
@@ -2,8 +2,8 @@ import type { IOrganization } from '@/interfaces/OrganizationInterface.js';
 import { model, Schema } from 'mongoose';
 
 const organizationSchema = new Schema<IOrganization>({
-  name: { type: String, required: true },
-  description: { type: String },
+  name: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
   createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 });
