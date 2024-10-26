be66a45e54338694fa6b90856bf7a3b007bb71f9
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-10-26 18:52:11 +0300

    Add new Interface to combine between two models

---
+++ src/interfaces/IOrganizationMemberInterface.ts
@@ -0,0 +1,7 @@
+import type { Role } from '@/enums/role.enum.js';
+import type { IUser } from './UserInterface.js';
+
+export interface IOrganizationMember {
+  user: IUser;
+  accessLevel: Role;
+}
