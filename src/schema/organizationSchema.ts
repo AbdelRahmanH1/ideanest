5ec7515f08380927b1f4eb7b73953652eeabd68e
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-10-26 21:55:07 +0300

    Add Organization Schema

---
+++ src/schema/organizationSchema.ts
@@ -0,0 +1,27 @@
+import type { IOrganization } from '@/interfaces/OrganizationInterface.js';
+import { isValidObjectId } from '@/middlewares/validation.js';
+import Joi from 'joi';
+
+export const createOrganizationSchema = Joi.object<IOrganization>({
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+}).required();
+
+export const updateOrganizationSchema = Joi.object<IOrganization>({
+  organization_id: Joi.string().custom(isValidObjectId).required(),
+  name: Joi.string(),
+  description: Joi.string(),
+}).required();
+
+export const deleteOrganizationSchema = Joi.object<IOrganization>({
+  organization_id: Joi.string().custom(isValidObjectId).required(),
+}).required();
+
+export const inviteToOrganizationSchema = Joi.object({
+  organization_id: Joi.string().custom(isValidObjectId).required(),
+  user_email: Joi.string().email().required(),
+}).required();
+
+export const getOrganizationByIDSchema = Joi.object<IOrganization>({
+  organization_id: Joi.string().custom(isValidObjectId).required(),
+}).required();
